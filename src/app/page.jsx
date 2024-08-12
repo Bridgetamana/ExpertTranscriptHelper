0404beaeaccb1e03c3d5b24c6d21a7dca5ceb78d
Lilianada <lilibest30.lb@gmail.com>
2024-08-12 22:34:39 +0100

    removed testimonial and updated testimonials but not using it

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,12 @@
 import { Footer } from '@/components/Footer'
 import Hero from '@/components/Hero'
 import { NavBar } from '@/components/NavBar'
-import { Pricing } from '@/components/Pricing'
+import { Packages } from '@/components/Packages'
 import { WhyChooseUs } from '@/components/WhyChooseUs'
 import { HowItWorks } from '@/components/HowItWorks'
 import { AccreditedSchools } from '@/components/AccreditedSchools'
 import Contact from '@/components/Contact'
 import YoutubeSection from '@/components/YoutubeSection'
-import { Testimonial } from '@/components/Testimonial'
-import { Testimonials } from '@/components/Testimonials'
 
 export default function Home() {
   return (
@@ -19,9 +17,8 @@ export default function Home() {
       <AccreditedSchools />
       <HowItWorks />
       <WhyChooseUs />
-      <Pricing />
+      <Packages />
       <Contact />
-      <Testimonials/>
       <Footer />
     </>
   )
