0c25bb700c132ae8c35373bb5eab307430c331c7
Bridget Amana <amanabridget03@gmail.com>
2024-08-14 07:58:42 +0100

    add faq section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,17 +7,21 @@ import { HowItWorks } from '@/components/HowItWorks'
 import { AccreditedSchools } from '@/components/AccreditedSchools'
 import Contact from '@/components/Contact'
 import YoutubeSection from '@/components/YoutubeSection'
+import AboutUs from '@/components/AboutUs'
+import Faq from '@/components/Faq'
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Hero />
+      <AboutUs />
       <YoutubeSection />
       <AccreditedSchools />
       <HowItWorks />
       <WhyChooseUs />
       <Packages />
+      <Faq />
       <Contact />
       <Footer />
     </>
